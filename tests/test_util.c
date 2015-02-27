d63536db6c249279607da844444fe8b81f4de632
javex <florian.ruechel@inexplicity.de>
2015-02-27 22:24:40 +0100

    Improve error test

--- tests/test_util.c
+++ tests/test_util.c
@@ -28,35 +28,9 @@ void teardown()
 START_TEST(test_scep_strerror)
 {
 	int i;
-	TEST_ERRMSG(SCEPE_OK, "No error");
-	TEST_ERRMSG(SCEPE_MEMORY, "Not enough memory available");
-	TEST_ERRMSG(SCEPE_INVALID_URL, "The given URL is invalid");
-	TEST_ERRMSG(SCEPE_UNKNOWN_CONFIGURATION, "This configuration option is "
-			"not known");
-	TEST_ERRMSG(SCEPE_UNKOWN_OPERATION, "Operation is unknown or no operation "
-			"specified");
-	TEST_ERRMSG(SCEPE_QUERY_OP, "The query key \"operation\" is a reserved "
-			"string and may not be used in query by the user");
-	TEST_ERRMSG(SCEPE_QUERY_PARSE, "There was an error while preparing the "
-			"query of the URL");
-	TEST_ERRMSG(SCEPE_MISSING_URL, "Missing URL configuration");
-	TEST_ERRMSG(SCEPE_MISSING_CSR, "You have to provide a CSR for the PKCSReq "
-			"operation");
-	TEST_ERRMSG(SCEPE_MISSING_REQ_KEY, "You have to provide the private key "
-			"for which you want a certificate");
-	TEST_ERRMSG(SCEPE_MISSING_CA_CERT, "The CA certificate is missing but is "
-			"needed to encrypt the message for the server and/or extract "
-			"certain values");
-	TEST_ERRMSG(SCEPE_MISSING_SIGKEY, "If you provide a signature "
-			"certificate, you also need to provide a signature key");
-	TEST_ERRMSG(SCEPE_MISSING_SIGCERT, "If you provide a signature key, you "
-			"also need to provide a signature certificate");
-	TEST_ERRMSG(SCEPE_MISSING_CERT_KEY, "To request an existing certificate "
-			"you need to provide the key for which it was created");
-	TEST_ERRMSG(SCEPE_MISSING_CRL_CERT, "To request a CRL you need to provide "
-			"the certificate which you want to validate");
-	TEST_ERRMSG(SCEPE_CURL, "cURL error. See error log for more details");
-	for(i=SCEPE_DUMMY_LAST_ERROR; i < 100; ++i)
+	for(i=SCEPE_OK; i < SCEPE_DUMMY_LAST_ERROR; i++)
+		ck_assert(strlen(scep_strerror(i)));
+	for(i=SCEPE_DUMMY_LAST_ERROR; i <= SCEPE_DUMMY_LAST_ERROR + 1; ++i)
 		TEST_ERRMSG(i, "Unknown error");
 }
 END_TEST
